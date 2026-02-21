58227470cd74b079bb328abf0dc99b6e74771755
ibiscum <ulf.luecking@gmail.com>
2026-02-21 23:13:11 +0100

    ch12

--- Chapter12/part2app/dist/server/data/index.js
+++
@@ -1,4 +0,0 @@
-//import { SqlRepository } from "./sql_repository";
-import { OrmRepository } from "./orm_repository.js";
-const repository = new OrmRepository();
-export default repository;
--- Chapter12/part2app/dist/server/data/orm_models.js
+++
@@ -1,7 +0,0 @@
-import { Model, } from "sequelize";
-export class Person extends Model {
-}
-export class Calculation extends Model {
-}
-export class ResultModel extends Model {
-}
