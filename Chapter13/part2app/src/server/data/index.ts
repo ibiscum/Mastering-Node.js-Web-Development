358326d33b9b64c6d992fb30a489cfa40065e380
ibiscum <ulf.luecking@gmail.com>
2026-02-21 23:15:10 +0100

    cache

--- Chapter13/part2app/src/server/data/index.ts
+++
@@ -1,6 +0,0 @@
-import { Repository } from "./repository";
-//import { SqlRepository } from "./sql_repository";
-import { OrmRepository } from "./orm_repository";
-
-const repository: Repository = new OrmRepository();
-export default repository;
