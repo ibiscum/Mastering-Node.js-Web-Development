a08a6a2e2002a2163707e05e4db2e7e8a25edb22
ibiscum <78886021+ibiscum@users.noreply.github.com>
2026-05-10 10:43:21 +0200

    update ch06 (#204)

--- Chapter06/webapp/dist/handler.js
+++
@@ -1,5 +0,0 @@
-import { readFileSync } from "fs";
-export const basicHandler = (req, resp) => {
-    resp.write(readFileSync("static/index.html"));
-    resp.end();
-};
--- Chapter06/webapp/dist/promises.js
+++
@@ -1,4 +0,0 @@
-import { ServerResponse } from "http";
-import { promisify } from "util";
-export const endPromise = promisify(ServerResponse.prototype.end);
-export const writePromise = promisify(ServerResponse.prototype.write);
